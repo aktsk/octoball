18cb12392a688d729f8f261a63b3283505033d95
Tomoki Sekiyama <tomoki.sekiyama@aktsk.jp>
2021-05-31 10:17:53 +0900

    Faster cached relation reads

--- lib/octoball/association.rb
+++ lib/octoball/association.rb
@@ -6,7 +6,7 @@ class Octoball
   end
 
   module ShardedCollectionAssociation
-    [:reader, :writer, :ids_reader, :ids_writer, :create, :create!,
+    [:writer, :ids_reader, :ids_writer, :create, :create!,
      :build, :include?, :load_target, :reload, :size, :select].each do |method|
       class_eval <<-"END", __FILE__, __LINE__ + 1
         def #{method}(*args, &block)
@@ -23,6 +23,20 @@ class Octoball
         end
         ruby2_keywords(:#{method}) if respond_to?(:ruby2_keywords, true)
       END
+    end
+  end
+
+  module ShardedCollectionProxyCreate
+    def create(klass, association)
+      shard = association.owner.current_shard
+      return super unless shard
+      return RelationProxy.new(super, shard) if shard == ActiveRecord::Base.current_shard
+      ret = nil
+      ActiveRecord::Base.connected_to(shard: shard, role: Octoball.current_role) do
+        ret = RelationProxy.new(super, shard)
+        nil # return nil to avoid loading relation
+      end
+      ret
     end
   end
 
@@ -43,7 +57,7 @@ class Octoball
   end
 
   module ShardedSingularAssociation
-    [:reader, :writer, :create, :create!, :build].each do |method|
+    [:reload, :writer, :create, :create!, :build].each do |method|
       class_eval <<-"END", __FILE__, __LINE__ + 1
         def #{method}(*args, &block)
           return super if !owner.current_shard || owner.current_shard == ActiveRecord::Base.current_shard
@@ -59,6 +73,7 @@ class Octoball
   ::ActiveRecord::Relation.prepend(RelationCurrentShard)
   ::ActiveRecord::QueryMethods::WhereChain.prepend(RelationCurrentShard)
   ::ActiveRecord::Associations::CollectionAssociation.prepend(ShardedCollectionAssociation)
+  ::ActiveRecord::Associations::CollectionProxy.singleton_class.prepend(ShardedCollectionProxyCreate)
   ::ActiveRecord::Associations::CollectionProxy.prepend(ShardedCollectionProxy)
   ::ActiveRecord::Associations::SingularAssociation.prepend(ShardedSingularAssociation)
 end
